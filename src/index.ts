1fa68b6bc2a3dab30e72ab82388ff5cb014b1664
Marcel <numax.cz@protonmail.ch>
2022-08-29 21:40:22 +0200

    Opraveny bugy

--- src/index.ts
+++ src/index.ts
@@ -287,7 +287,7 @@ export class NapicuDate {
    * Gets the current day name
    */
   public getCurrentDayName(): string {
-    return NapicuDateConfig.days[this._date.getDay() - 1];
+    return NapicuDateConfig.days[this._date.getDate() - 1];
   }
 
   /**
